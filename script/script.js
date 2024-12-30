f5c4e5be0ebf6aeb485db65c8b47cf767ec0f0eb
yan <academiatkj@gmail.com>
2024-12-31 01:55:16 +0800

    update bahasa dan add slogan

--- script/script.js
+++ script/script.js
@@ -12,3 +12,19 @@ window.addEventListener('scroll', function () {
 //     duration: 1000, // Durasi animasi dalam milidetik
 //     once: false // Animasi terjadi
 // });
+
+// Function to scroll to the top
+function scrollToTop() {
+  window.scrollTo({ top: 0, behavior: 'smooth' });
+}
+
+// Show or hide the button based on scroll position
+window.onscroll = function() {
+  const button = document.querySelector('.back-to-top');
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+      button.style.display = 'block';
+  } else {
+      button.style.display = 'none';
+  }
+};
+
